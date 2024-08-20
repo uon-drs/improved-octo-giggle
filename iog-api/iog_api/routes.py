206e6576aee7814c710073a4c6bd64789fb89447
James Mitchell-White <jamesjimitchell@gmail.com>
2024-08-20 11:57:14 +0100

    Added the /checks GET endpoint
    For now, the /checks endpoint only sends back the builtin checks that take a single parameter

--- iog-api/iog_api/routes.py
+++ iog-api/iog_api/routes.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -5,3 +6,30 @@ router = APIRouter()
 @router.get("/dtypes")
 async def get_dtypes():
     return ["Some dtypes here"]
+
+@router.get("/checks")
+async def get_checks() -> List:
+    """
+    Sends a list of the supported checks
+
+    Returns
+    -------
+    List
+        A list of pandera's built in checks. We're only using the checks that take a single argument
+    """
+    return [
+            "equal to",
+            "not equal to"
+            "greater than",
+            "greater than or equal to",
+            "less than",
+            "less than or equal to",
+            "is in",
+            "is not in",
+            "string contains",
+            "string ends with",
+            # "string length" takes two args,
+            "string starts with",
+            "string matches",
+            "unique values equal"
+            ]
