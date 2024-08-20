266000482158c2f2c4e9f4ed46d7fed3d4ea640e
James Mitchell-White <jamesjimitchell@gmail.com>
2024-08-20 11:10:00 +0100

    Set up the fastapi
    you can start the server with
    `fastapi run main.py`

---
+++ iog-api/iog_api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from . import routes
+
+app = FastAPI(
+        title="improved-octo-giggles",
+        version="0.1.0",
+        summary="Validate your data here",
+        )
+
+app.include_router(routes.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Bring out yer data"}
