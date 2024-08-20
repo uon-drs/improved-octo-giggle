0c9ae131bb2a00689bec3a8438f8be36e5236217
Kaung <kaungmyatwaiyan.4869@gmail.com>
2024-08-20 13:50:49 +0100

    Add validate_service.py

---
+++ iog-api/iog_api/services/validate_service.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import pandera as pa
+
+def validate_schema(schema: pa.DataFrameSchema, data: pd.DataFrame):
+    try:
+        # Attempt to validate the data against the schema
+        schema.validate(data, lazy=True)
+        return True
+    except pa.errors.SchemaError as exc:
+        print(exc)
+        return False
+    
+    
+
+
