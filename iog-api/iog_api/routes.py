612942f0ce55c8e077db681327ebd25c4a0b54ed
itishreya <shreyaiyer1998@gmail.com>
2024-08-20 13:06:46 +0100

    added a list of datatypes

--- iog-api/iog_api/routes.py
+++ iog-api/iog_api/routes.py
@@ -1,7 +1,34 @@
 from fastapi import APIRouter
+from typing import List
+
 
 router = APIRouter()
 
-@router.get("/dtypes")
-async def get_dtypes():
-    return ["Some dtypes here"]
+data_types = [
+    "bool",
+    "datetime64[ns]",
+    "timedelta64[ns]",
+    "category",
+    "float16",
+    "float32",
+    "float64",
+    "float128",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "uint8",
+    "uint16",
+    "uint32",
+    "uint64",
+    "complex64",
+    "complex128",
+    "complex256",
+    "decimal",
+    "string"
+]
+
+@router.get("/datatypes/", response_model=List[str])
+async def get_data_types():
+    return data_types
+
