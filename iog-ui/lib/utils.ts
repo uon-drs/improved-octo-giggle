1c123379090b9385fd4452287acfafb7ebb14c32
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2024-09-20 16:17:36 +0100

    function that should really be in the JS standard lib lol

--- iog-ui/lib/utils.ts
+++ iog-ui/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function capitalise(word: string): string {
+  const lower = word.toLowerCase();
+  return word.charAt(0).toUpperCase() + lower.slice(1);
+}
